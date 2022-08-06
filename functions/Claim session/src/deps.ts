b4b5597dbe72d178bff5c6837f585fe59cdfd2e0
Niels <56843328+Nierot@users.noreply.github.com>
2022-08-06 21:08:00 +0200

    📦 NEW: Claim a session

---
+++ functions/Claim session/src/deps.ts
@@ -0,0 +1,2 @@
+export * as sdk from "https://deno.land/x/appwrite@5.0.1/mod.ts";
+export * as sipapu from "https://esm.sh/sipapu-2@latest";
---
+++ functions/Claim session/src/mod.ts
@@ -0,0 +1,92 @@
+import { Query } from "https://deno.land/x/appwrite@5.0.1/mod.ts";
+import { sdk, sipapu } from "./deps.ts";
+
+/*
+  'req' variable has:
+    'headers' - object with request headers
+    'payload' - object with request body data
+    'env' - object with environment variables
+
+  'res' variable has:
+    'send(text, status)' - function to return text response. Status code defaults to 200
+    'json(obj, status)' - function to return JSON response. Status code defaults to 200
+  
+  If an error is thrown, a response with code 500 will be returned.
+*/
+
+// deno-lint-ignore no-explicit-any
+export default async function (req: any, res: any) {
+  const client = new sdk.Client();
+
+  // You can remove services you don't use
+  const database = new sdk.Databases(client, 'main');
+
+  if (!req.env['APPWRITE_FUNCTION_ENDPOINT'] || !req.env['APPWRITE_FUNCTION_API_KEY']) {
+    console.warn("Environment variables are not set. Function cannot use Appwrite SDK.");
+  } else {
+    client
+      .setEndpoint(req.env['APPWRITE_FUNCTION_ENDPOINT'] as string)
+      .setProject(req.env['APPWRITE_FUNCTION_PROJECT_ID'] as string)
+      .setKey(req.env['APPWRITE_FUNCTION_API_KEY'] as string);
+  }
+
+  const payload = JSON.parse(req.payload)
+
+  if (!payload.session_id) {
+    return res.json({
+      status: 500,
+      error: 'session_id missing from request payload'
+    })
+  }
+
+  if (!payload.playlist_id) {
+    return res.json({
+      status: 500,
+      error: 'playlist_id missing from request payload'
+    })
+  }
+
+  if (!payload.user_id) {
+    return res.json({
+      status: 500,
+      error: 'user_id missing from request payload'
+    })
+  }
+
+  try {
+    const session = await database.listDocuments('temp_session', [
+      Query.equal('session_id', payload.session_id)
+    ])
+
+    if (session.total === 0) {
+      return res.json({
+        status: 400,
+        message: 'No sessions found with that session_id'
+      })
+    }
+
+    // since there can only be one session with that id, select the first one
+    const session_id = session.documents[0].$id
+  
+    await database.createDocument('session', session_id, {
+      settings: JSON.stringify(sipapu.DEFAULT_SETTINGS),
+      playlist_id: payload.playlist_id,
+      user_id: payload.user_id
+    })
+
+    await database.deleteDocument('temp_session', session_id)
+
+    return res.json({
+      status: 200,
+      session_id: session_id
+    })
+
+  } catch (err) {
+    
+    return res.json({
+      status: 500,
+      message: err.message
+    })
+  
+  }
+}
